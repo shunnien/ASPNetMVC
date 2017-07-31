257b0731d6c661751e0b0f155d6846560736d211
allen_yu <wiamyu@gmail.com>
2017-07-31 15:03:31 +0800

    update order_details metadata

--- ModelExt/Models/Metadata/Order_Details.cs
+++ ModelExt/Models/Metadata/Order_Details.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace ModelExt.Models.Metadata
+namespace ModelExt.Models
 {
-    public class Order_Details
+    [MetadataType(typeof(Order_DetailsMetadata))]
+    public partial class Order_Details
     {
+        public class Order_DetailsMetadata
+        {
+            [DisplayName("單價")]
+            public decimal UnitPrice { get; set; }
+            [DisplayName("數量")]
+            public short Quantity { get; set; }
+            [DisplayName("折扣")]
+            public float Discount { get; set; }
+        }
     }
 }
