5eced6be95bac133f1f0bb0d5afd89cf942ffcb4
allen_yu <wiamyu@gmail.com>
2017-07-31 10:16:50 +0800

    add model extension method

---
+++ ModelExt/Utility/ModelExtMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ModelExt.Models;
+
+namespace ModelExt.Utility
+{
+    public static class ModelExtMethod
+    {
+        public static decimal TotalPrice(this Order_Details orderDetail)
+        {
+            decimal result = 0;
+            result = orderDetail.UnitPrice * orderDetail.Quantity;
+            return result;
+        }
+    }
+}
