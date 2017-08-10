26f50b5bc064656f3f32066418a1b9f829f0da5c
allen_yu <wiamyu@gmail.com>
2017-08-10 10:35:45 +0800

    create Identity Entity

---
+++ IdentityDbFirst/Models/IdentityEntity.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityDbFirst.Models
+{
+    /// <summary>
+    /// edmx IdentityUser 資料表類別對應 Identity 套件的使用者資料
+    /// </summary>
+    /// <seealso cref="int" />
+    public partial class IdentityUser: IUser<int>
+    {
+        /// <summary>
+        /// 非同步產生使用者驗證
+        /// </summary>
+        /// <param name="manager">Identity 的 UserManager</param>
+        /// <returns>Task&lt;ClaimsIdentity&gt;.</returns>
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<IdentityUser, int> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    /// <summary>
+    /// edmx IdentityRole 資料表類別對應 Identity 套件的角色資料
+    /// </summary>
+    /// <seealso cref="int" />
+    public partial class IdentityRole : IRole<int>
+    {
+    }
+}
